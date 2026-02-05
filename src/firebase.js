6f1582bae4332da6feac1c5116b99bc9229ae449
varun-program <varunyt.sai@outlook.com>
2026-02-06 01:12:34 +0530

    final 1.2

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyD74CxOZrzIu2kVO5aD5G7VAqFYpBDVNpY",
   authDomain: "event-97a74.firebaseapp.com",
@@ -12,25 +11,6 @@ const firebaseConfig = {
   appId: "1:467454908284:web:4c0b484fbd6b6bbbd4f6f9"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
-
-rules_version = '2';
-service cloud.firestore {
-  match /databases/{database}/documents {
-
-    // Public read: events
-    match /events/{doc} {
-      allow read: if true;
-      allow write: if request.auth != null;
-    }
-
-    // Public write: registrations
-    match /registrations/{doc} {
-      allow create: if true;
-      allow read, update, delete: if request.auth != null;
-    }
-  }
-}
